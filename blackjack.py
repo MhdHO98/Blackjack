602a28726f2f22d1a2e423bf9e7a41c07a3249bc
MhdHO98 <167417592+MhdHO98@users.noreply.github.com>
2024-05-06 19:28:11 +0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,26 @@
+import random
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(rank, suit) for rank in self._get_ranks() for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]]
+
+    def _get_ranks(self):
+        return [str(n) for n in range(1, 11)] + ['Jack', 'Queen', 'King']
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def get_card(self):
+        return self.cards.pop() if self.cards else None
+        
 class BlackJackGame:
     def __init__(self, num_players):
         self.players = []
@@ -49,3 +72,5 @@ class BlackJackGame:
             return 11 if sum([self.get_card_value(c) for c in hand_cards]) <= 10 else 1
         else:
             return int(card.rank)
+game = BlackJackGame(2)
+game.play()
